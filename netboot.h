434e854202236ec5809dbb96589fc34313dbff9e
Andrew Boie <andrew.p.boie@intel.com>
2013-10-31 13:56:56 -0700

    netboot.h: fix build error on 32-bit systems
    Function prototype/implementation mismatch.
    
    Change-Id: I89aaae1b49d0372d3aed76fc21c194e0ae55f72e
    Signed-off-by: Andrew Boie <andrew.p.boie@intel.com>

--- netboot.h
+++ netboot.h
@@ -5,5 +5,5 @@ extern BOOLEAN findNetboot(EFI_HANDLE image_handle);
 
 extern EFI_STATUS parseNetbootinfo(EFI_HANDLE image_handle);
 
-extern EFI_STATUS FetchNetbootimage(EFI_HANDLE image_handle, VOID **buffer, UINTN *bufsiz);
+extern EFI_STATUS FetchNetbootimage(EFI_HANDLE image_handle, VOID **buffer, UINT64 *bufsiz);
 #endif
