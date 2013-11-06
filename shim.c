rhboot/shim

commit 556c445ea19fc257fe35ac1a67477e7352ba3fcd

Don't free GetVariable() return data without checking the status code.
This breaks every machine from before Secure Boot was a thing.

Signed-off-by: Peter Jones <pjones@redhat.com>

shim.c
@@ -456,21 +456,30 @@ static BOOLEAN secure_mode (void)
 		return FALSE;
 
 	status = get_variable(L"SecureBoot", &Data, &len, global_var);
+	if (status != EFI_SUCCESS) {
+		if (verbose)
+			console_notify(L"Secure boot not enabled\n");
+		return FALSE;
+	}
 	sb = *Data;
 	FreePool(Data);
 
-	/* FIXME - more paranoia here? */
-	if (status != EFI_SUCCESS || sb != 1) {
+	if (sb != 1) {
 		if (verbose)
 			console_notify(L"Secure boot not enabled\n");
 		return FALSE;
 	}
 
 	status = get_variable(L"SetupMode", &Data, &len, global_var);
+	if (status == EFI_SUCCESS) {
+		if (verbose)
+			console_notify(L"Platform is in setup mode\n");
+		return FALSE;
+	}
 	setupmode = *Data;
 	FreePool(Data);
 
-	if (status == EFI_SUCCESS && setupmode == 1) {
+	if (setupmode == 1) {
 		if (verbose)
 			console_notify(L"Platform is in setup mode\n");
 		return FALSE;

commit ed1b34abd7da220dc8689cde0845e542d0d30551

Fix detection of SetupMode in secure_mode
This was broken by commit 556c445ea19fc257fe35ac1a67477e7352ba3fcd. The
previous code assumes setup mode even if just the SetupMode variable is
present but not set to 1. If get_variable returns EFI_SUCCESS, this only
means that the variable was read successfully and does not tell anything
about it's value. And as we already checked if secure boot is enabled, the
function should return TRUE anyway.

This is a security fix as in setup mode shim will load unsigned code!

shim.c
@@ -472,17 +472,14 @@ static BOOLEAN secure_mode (void)
 
 	status = get_variable(L"SetupMode", &Data, &len, global_var);
 	if (status == EFI_SUCCESS) {
-		if (verbose)
-			console_notify(L"Platform is in setup mode\n");
-		return FALSE;
-	}
-	setupmode = *Data;
-	FreePool(Data);
+		setupmode = *Data;
+		FreePool(Data);
 
-	if (setupmode == 1) {
-		if (verbose)
-			console_notify(L"Platform is in setup mode\n");
-		return FALSE;
+		if (setupmode == 1) {
+			if (verbose)
+				console_notify(L"Platform is in setup mode\n");
+			return FALSE;
+		}
 	}
 
 	return TRUE;