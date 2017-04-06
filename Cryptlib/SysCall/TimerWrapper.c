c68cbf6a28fed400cda7780ba0a38b60d4092778
Gary Lin <glin@suse.com>
2017-04-06 15:54:00 +0800

    Cryptlib: Update to the latest edk2 commit
    - Update to edk2 commit 7c410b3d4180087020c7734bf67cdc4ad9fdb136
      CryptoPkg/BaseCryptLib: Adding NULL checking in time() wrapper.
    
    - Update headers in Cryptlib/Include/openssl/ to 1.1.0e
      + Also copy the openssl internal headers
    
    Signed-off-by: Gary Lin <glin@suse.com>

--- Cryptlib/SysCall/TimerWrapper.c
+++ Cryptlib/SysCall/TimerWrapper.c
@@ -2,7 +2,7 @@
   C Run-Time Libraries (CRT) Time Management Routines Wrapper Implementation
   for OpenSSL-based Cryptographic Library (used in DXE & RUNTIME).
 
-Copyright (c) 2010 - 2016, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2010 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
