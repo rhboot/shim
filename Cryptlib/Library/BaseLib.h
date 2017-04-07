898d69a147b86c491559aa5239564d5cd994e8b7
Gary Lin <glin@suse.com>
2017-04-07 12:32:24 +0800

    Cryptlib: amend the headers and fix signness
    - Declare some functions in the proper headers
      + We missed them for a long time...
    
    - Cast offsetof to UINTN
      + The original casting triggers the gcc warning since int can not
        present the offset for the 64bit machines.
    
    - Cast the "char" array to "CHAR8 *" to avoid the gcc warnings
    
    - Implement atoi correctly
    
    Signed-off-by: Gary Lin <glin@suse.com>

--- Cryptlib/Library/BaseLib.h
+++ Cryptlib/Library/BaseLib.h
@@ -1,2 +1,8 @@
 #include <efi.h>
 #include <efilib.h>
+
+UINT32 WriteUnaligned32 (UINT32 *Buffer, UINT32 Value);
+UINTN AsciiStrSize (CHAR8 *string);
+char *AsciiStrnCpy(char *Destination, char *Source, UINTN count);
+char *AsciiStrCat(char *Destination, char *Source);
+UINTN AsciiStrDecimalToUintn(const char *String);
