19a7e1bcd561d735ed41d22e12679e70f5c90b1c
Peter Jones <pjones@redhat.com>
2017-09-08 15:12:16 -0400

    Add some defaults and a syntastic file so vim helps more.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- shim.h
+++ shim.h
@@ -37,3 +37,63 @@ typedef struct _SHIM_LOCK {
 
 extern EFI_STATUS shim_init(void);
 extern void shim_fini(void);
+
+#ifdef __x86_64__
+#ifndef DEFAULT_LOADER
+#define DEFAULT_LOADER L"\\grubx64.efi"
+#endif
+#ifndef DEFAULT_LOADER_CHAR
+#define DEFAULT_LOADER_CHAR L"\\grubx64.efi"
+#endif
+#ifndef EFI_ARCH
+#define EFI_ARCH L"x64"
+#endif
+#ifndef DEBUGDIR
+#define DEBUGDIR L"/usr/lub/debug/usr/share/shim/x64/"
+#endif
+#endif
+
+#if defined(__i686__) || defined(__i386__)
+#ifndef DEFAULT_LOADER
+#define DEFAULT_LOADER L"\\grubia32.efi"
+#endif
+#ifndef DEFAULT_LOADER_CHAR
+#define DEFAULT_LOADER_CHAR L"\\grubia32.efi"
+#endif
+#ifndef EFI_ARCH
+#define EFI_ARCH L"ia32"
+#endif
+#ifndef DEBUGDIR
+#define DEBUGDIR L"/usr/lub/debug/usr/share/shim/ia32/"
+#endif
+#endif
+
+#if defined(__aarch64__)
+#ifndef DEFAULT_LOADER
+#define DEFAULT_LOADER L"\\grubaa64.efi"
+#endif
+#ifndef DEFAULT_LOADER_CHAR
+#define DEFAULT_LOADER_CHAR L"\\grubaa64.efi"
+#endif
+#ifndef EFI_ARCH
+#define EFI_ARCH L"aa64"
+#endif
+#ifndef DEBUGDIR
+#define DEBUGDIR L"/usr/lub/debug/usr/share/shim/aa64/"
+#endif
+#endif
+
+#if defined(__arm__)
+#ifndef DEFAULT_LOADER
+#define DEFAULT_LOADER L"\\grubarm.efi"
+#endif
+#ifndef DEFAULT_LOADER_CHAR
+#define DEFAULT_LOADER_CHAR L"\\grubarm.efi"
+#endif
+#ifndef EFI_ARCH
+#define EFI_ARCH L"arm"
+#endif
+#ifndef DEBUGDIR
+#define DEBUGDIR L"/usr/lub/debug/usr/share/shim/arm/"
+#endif
+#endif
