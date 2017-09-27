c7a685ee52a7b09ce3457899533045170dd68bb1
Peter Jones <pjones@redhat.com>
2017-09-27 10:17:45 -0400

    MokManager: check_der_suffix(): use StrnCat() on our suffix checker.
    We know it's legit already because we computed the pointer from the end,
    but covscan gets confused, and we have StrnCat, so we should just use it
    anyway.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- MokManager.c
+++ MokManager.c
@@ -1960,7 +1960,7 @@ static BOOLEAN check_der_suffix(CHAR16 * file_name)
 		return FALSE;
 
 	suffix[0] = '\0';
-	StrCat(suffix, file_name + StrLen(file_name) - 4);
+	StrnCat(suffix, file_name + StrLen(file_name) - 4, 4);
 
 	StrLwr(suffix);
 	for (i = 0; der_suffix[i] != NULL; i++) {
