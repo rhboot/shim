ca1d0534faa9382ce09641c4e697223a47851a8f
Peter Jones <pjones@redhat.com>
2017-10-20 11:37:37 -0400

    CryptLib: Add the AsciiStrCpy() decl.
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- Cryptlib/Library/BaseLib.h
+++ Cryptlib/Library/BaseLib.h
@@ -5,4 +5,5 @@ UINT32 WriteUnaligned32 (UINT32 *Buffer, UINT32 Value);
 UINTN AsciiStrSize (CHAR8 *string);
 char *AsciiStrnCpy(char *Destination, char *Source, UINTN count);
 char *AsciiStrCat(char *Destination, char *Source);
+CHAR8 *AsciiStrCpy(CHAR8 *Destination, CHAR8 *Source);
 UINTN AsciiStrDecimalToUintn(const char *String);
