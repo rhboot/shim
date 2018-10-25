81a32252f5246380262ee4c823a5c25fe083fd3c
Peter Jones <pjones@redhat.com>
2018-10-25 11:18:57 -0400

    Makefiles: re-organize and simplify
    This does a couple of things:
    - Fixes our make variable inheritence and makes it so we don't have to
      pass things on the make command line about subdirs and whatnot
    - Removes all the duplicated variables from the subdirs
    - Rename the makefiles so editors guess syntax better and whatnot:
      Make.(.*) -> include/\1.mk
    - Keeps the linker scripts with the individual makefiles to un-clutter
      the top dir a bit.
    - Gets rid of the thoroughly pointless Make.rules; it's in defaults.mk
      now.
    - Make per-arch makefiles and switch more stuff to EFI arch names
    - Reorganize the way openssl CFLAGS work
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

---
+++ include/arch-aa64.mk
@@ -0,0 +1,21 @@
+#
+# arch-arm.mk
+# Peter Jones, 2018-11-09 15:28
+#
+ifeq ($(ARCH),aa64)
+
+ARCH_CFLAGS ?= -mstrict-align \
+	       -DMDE_CPU_AARCH64 \
+	       -DPAGE_SIZE=4096 -DPAGE_SHIFT=12
+ARCH_LDFLAGS += --defsym=EFI_SUBSYSTEM=$(SUBSYSTEM)
+ARCH_SUFFIX ?= aa64
+ARCH_SUFFIX_UPPER ?= AA64
+FORMAT := -O binary
+LDARCH ?= aarch64
+LIBDIR ?= $(prefix)/lib64
+SUBSYSTEM := 0xa
+TIMESTAMP_LOCATION := 72
+
+endif
+#
+# vim:ft=make
---
+++ include/arch-x64.mk
@@ -0,0 +1,22 @@
+#
+# arch-x64.mk
+# Peter Jones, 2018-11-09 15:24
+#
+ifeq ($(ARCH),x64)
+
+ARCH_CFLAGS ?= -mno-mmx -mno-sse -mno-red-zone -nostdinc \
+	       $(CLANG_BUGS) -m64 \
+	       -DEFI_FUNCTION_WRAPPER -DGNU_EFI_USE_MS_ABI \
+	       -DNO_BUILTIN_VA_FUNCS -DMDE_CPU_X64 \
+	       -DPAGE_SIZE=4096 -DPAGE_SHIFT=12
+ARCH_LDFLAGS ?=
+ARCH_SUFFIX ?= x64
+ARCH_SUFFIX_UPPER ?= X64
+CLANG_BUGS = $(if $(findstring gcc,$(CC)),-maccumulate-outgoing-args,)
+LDARCH ?= x86_64
+LIBDIR ?= $(prefix)/lib64
+TIMESTAMP_LOCATION := 136
+
+endif
+#
+# vim:ft=make
