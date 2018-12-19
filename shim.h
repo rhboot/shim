e0679b127a175ec2c5c1bfd638b5840834c5a3bc
Gary Lin <glin@suse.com>
2018-12-19 11:27:42 +0800

    shim: only include shim_cert.h in shim.c
    The shim_cert array was declared as a static array, and every user of
    shim_cert.h would create a shim_cert array for its own and grow the file
    size. To remove the unnecessary duplicate shim_cert arrays, this commit
    declares shim_cert in shim.c while other users still can access the
    array through the external variables: build_cert and build_cert_size.
    
    Signed-off-by: Gary Lin <glin@suse.com>

--- shim.h
+++ shim.h
@@ -120,9 +120,6 @@
 #include "include/variables.h"
 
 #include "version.h"
-#ifdef ENABLE_SHIM_CERT
-#include "shim_cert.h"
-#endif
 
 INTERFACE_DECL(_SHIM_LOCK);
 
@@ -170,6 +167,10 @@ extern UINT32 vendor_cert_size;
 extern UINT32 vendor_dbx_size;
 extern UINT8 *vendor_cert;
 extern UINT8 *vendor_dbx;
+#if defined(ENABLE_SHIM_CERT)
+extern UINT32 build_cert_size;
+extern UINT8 *build_cert;
+#endif /* defined(ENABLE_SHIM_CERT) */
 
 extern UINT8 user_insecure_mode;
 extern UINT8 ignore_db;
