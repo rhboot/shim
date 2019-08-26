73a6501b5e0c381826e048d754b3dd3e4b577e98
Peter Jones <pjones@redhat.com>
2019-08-26 15:14:21 -0400

    Better cryptlib and openssl updater

--- Cryptlib/OpenSSL/update.sh
+++ Cryptlib/OpenSSL/update.sh
@@ -10,107 +10,48 @@ usage() {
 [[ $# -eq 1 ]] || usage
 [[ -n "${1}" ]] || usage
 
-DIR=$1
+DIR="${1}"
 
-WORK_PATH=$PWD
-OPENSSLLIB_PATH=$DIR/CryptoPkg/Library/OpensslLib
-OPENSSL_PATH=$OPENSSLLIB_PATH/openssl
+WORK_PATH="${PWD}"
+OPENSSLLIB_PATH="${DIR}/CryptoPkg/Library/OpensslLib"
+OPENSSL_PATH="${OPENSSLLIB_PATH}/openssl"
 
-cd $OPENSSLLIB_PATH
+needcommit() {(
+    pwd
+    cd "${DIR}"
+    if ! git diff-files --name-only -z | xargs -r -0 -n1 git add ; then
+        pwd
+        "${@}"
+    fi
+)}
+
+cd "${OPENSSL_PATH}"
+
+cd "${OPENSSLLIB_PATH}"
 perl -I. -Iopenssl/ process_files.pl
-cd $DIR
-git add -A CryptoPkg
-git add CryptoPkg/Library/OpensslLib/openssl/include/openssl/opensslconf.h
-git add CryptoPkg/Library/OpensslLib/openssl/configdata.pm
-git add CryptoPkg/Library/OpensslLib/openssl/Makefile
-git commit -m "Update openssl configs"
-cd $WORK_PATH
+set -x
+cd openssl
+pwd
+needcommit git commit -a -m "Update OpenSSL"
+cd ..
+pwd
+needcommit git add -A . openssl
+needcommit git commit -m "Update OpenSSL" openssl
 
-cp $OPENSSLLIB_PATH/buildinf.h buildinf.h
-cp $OPENSSL_PATH/e_os.h e_os.h
+cd "${DIR}"
+pwd
+needcommit git commit -m "Update CryptoPkg and openssl" "${OPENSSL_PATH}" CryptoPkg
+cd "${WORK_PATH}"
+
+rsync -avSHP "${OPENSSLLIB_PATH}/buildinf.h" buildinf.h
+rsync -avSHP "${OPENSSL_PATH}/e_os.h" e_os.h
+git add buildinf.h e_os.h
 
 mkdir -p crypto
-C_FILES="
-	LPdir_nyi.c
-	cpt_err.c
-	cryptlib.c
-	cversion.c
-	ebcdic.c
-	ex_data.c
-	init.c
-	mem.c
-	mem_clr.c
-	mem_dbg.c
-	mem_sec.c
-	o_dir.c
-	o_fips.c
-	o_fopen.c
-	o_init.c
-	o_str.c
-	o_time.c
-	threads_none.c
-	threads_pthread.c
-	threads_win.c uid.c
-"
-for file in $C_FILES
-do
-	cp $OPENSSL_PATH/crypto/$file crypto
-done
+rsync -avSHP --delete-during "${OPENSSL_PATH}"/crypto/ crypto/
 
-SUBDIRS="
-	include/internal/
-	aes
-	asn1
-	async/arch
-	async
-	bio
-	bn
-	buffer
-	cmac
-	comp
-	conf
-	dh
-	dso
-	err
-	evp
-        fips
-	hmac
-	kdf
-	lhash
-	md5
-	modes
-	objects
-	ocsp
-	pem
-	pkcs12
-	pkcs7
-	rand
-	rc4
-	rsa
-	sha
-	stack
-	txt_db
-	x509
-	x509v3
-"
-for dir in $SUBDIRS
-do
-	mkdir -p crypto/$dir
-	cp $OPENSSL_PATH/crypto/$dir/*.[ch] crypto/$dir
-done
+git clean -f -d -X -- crypto/
 
-# Remove unused files
-rm -f crypto/aes/aes_x86core.c
-rm -f crypto/x509v3/tabtest.c
-rm -f crypto/x509v3/v3conf.c
-rm -f crypto/x509v3/v3prin.c
+git add crypto/
 
-find . -name "*.[ch]" -exec chmod -x {} \;
-
-git add -A .
-git commit -m "Update OpenSSL"
-
-git config --local --add am.keepcr true
-git am \
-    0001-OpenSSL-bio-b_print-disable-SSE.patch \
-    0002-OpenSSL-Pk7-smime-add-an-error-message.patch
+needcommit git commit -a -m "Update OpenSSL"
--- Makefile
+++ Makefile
@@ -22,6 +22,25 @@ mkbuilddir :
 	@mkdir -p $(BUILDDIR)
 	@ln -f $(TOPDIR)/include/build.mk $(BUILDDIR)/Makefile
 
-.PHONY: mkbuilddir
+update : TOPDIR=$(shell pwd)
+update :
+	cd $(TOPDIR) ; \
+		git submodule sync --recursive
+	cd $(TOPDIR)/edk2/CryptoPkg/Library/OpensslLib/openssl ; \
+		git fetch rhboot ; \
+		git rebase rhboot/shim
+	cd $(TOPDIR)/edk2/CryptoPkg/Library/OpensslLib ; \
+		if ! git diff-index --quiet HEAD -- openssl ; then \
+			git commit -m "Update openssl" openssl ; \
+		fi
+	cd $(TOPDIR)/edk2 ; git fetch rhboot ; git rebase rhboot/shim
+	cd $(TOPDIR) ; \
+		if ! git diff-index --quiet HEAD -- edk2 ; then \
+			git commit -m "Update edk2" edk2 ; \
+		fi
+	cd $(TOPDIR)/Cryptlib ; \
+		./update.sh
+
+.PHONY: mkbuilddir update
 # vim:ft=make
 #
