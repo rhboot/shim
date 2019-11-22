bb521a9f2890d8478c10efc7387a32bce42321e9
Gary Lin <glin@suse.com>
2019-11-22 17:22:15 +0800

    lib: move print_crypto_errors() out of console.c
    print_crypto_errors() will pull in the whole openssl library which
    bloats the size of fallback.efi. Move the function to an independent
    file (lib/print_crypto.c) to reduce the file size of fallback.efi from
    1.3MB to 93KB.
    
    Signed-off-by: Gary Lin <glin@suse.com>

---
+++ lib/print_crypto.c
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: BSD-2-Clause-Patent
+/*
+ * Copyright 2019 SUSE LLC <glin@suse.com>
+ */
+
+#include <efi.h>
+#include <efilib.h>
+#include <stdarg.h>
+
+#include "shim.h"
+
+#include <Library/BaseCryptLib.h>
+#include <openssl/err.h>
+#include <openssl/crypto.h>
+#include <console.h>
+
+static int
+print_errors_cb(const char *str, size_t len, void *u)
+{
+	console_print(L"%a", str);
+
+	return len;
+}
+
+EFI_STATUS
+print_crypto_errors(EFI_STATUS efi_status,
+		    char *file, const char *func, int line)
+{
+	if (!(verbose && EFI_ERROR(efi_status)))
+		return efi_status;
+
+	console_print(L"SSL Error: %a:%d %a(): %r\n", file, line, func,
+		      efi_status);
+	ERR_print_errors_cb(print_errors_cb, NULL);
+
+	return efi_status;
+}
