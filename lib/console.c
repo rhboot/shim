6b29eb80512fabb1396ce8b96bddbee0fb8ea70d
Nikolai Kostrigin <nickel@altlinux.org>
2019-12-02 10:24:08 +0300

    Fix typo in lib/console.c EFI_WARN_UNKNOWN_GLYPH definition

--- lib/console.c
+++ lib/console.c
@@ -520,7 +520,7 @@ static struct {
 	{  EFI_SECURITY_VIOLATION,     L"Security Violation"},
 
 	// warnings
-	{  EFI_WARN_UNKOWN_GLYPH,      L"Warning Unknown Glyph"},
+	{  EFI_WARN_UNKNOWN_GLYPH,      L"Warning Unknown Glyph"},
 	{  EFI_WARN_DELETE_FAILURE,    L"Warning Delete Failure"},
 	{  EFI_WARN_WRITE_FAILURE,     L"Warning Write Failure"},
 	{  EFI_WARN_BUFFER_TOO_SMALL,  L"Warning Buffer Too Small"},
