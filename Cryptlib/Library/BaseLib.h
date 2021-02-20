b8138e5d45b4695dfdf5c08d68da32788635ed1e
Peter Jones <pjones@redhat.com>
2021-02-20 12:32:48 -0500

    Cryptlib: make some Str*() args const.

--- Cryptlib/Library/BaseLib.h
+++ Cryptlib/Library/BaseLib.h
@@ -2,8 +2,8 @@
 #include <efilib.h>
 
 UINT32 WriteUnaligned32 (UINT32 *Buffer, UINT32 Value);
-UINTN AsciiStrSize (CHAR8 *string);
-char *AsciiStrnCpy(char *Destination, char *Source, UINTN count);
-char *AsciiStrCat(char *Destination, char *Source);
-CHAR8 *AsciiStrCpy(CHAR8 *Destination, CHAR8 *Source);
-UINTN AsciiStrDecimalToUintn(const char *String);
+UINTN AsciiStrSize (const CHAR8 *string);
+CHAR8 *AsciiStrnCpy(CHAR8 *Destination, const CHAR8 *Source, UINTN count);
+CHAR8 *AsciiStrCat(CHAR8 *Destination, const CHAR8 *Source);
+CHAR8 *AsciiStrCpy(CHAR8 *Destination, const CHAR8 *Source);
+UINTN AsciiStrDecimalToUintn(const CHAR8 *String);
