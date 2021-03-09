9beca885c29c77bb901547321a5ce6fd3c9c8ee3
Peter Jones <pjones@redhat.com>
2021-03-09 14:40:03 -0500

    Fix stdarg to work the same everywhere.
    This gets us the same working definition for VA_* va_* etc everywhere,
    and it's the same definition edk2 is using.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- errlog.c
+++ errlog.c
@@ -10,24 +10,26 @@ static CHAR16 **errs = NULL;
 static UINTN nerrs = 0;
 
 EFI_STATUS EFIAPI
-vdprint_(const CHAR16 *fmt, const char *file, int line, const char *func, elf_va_list args)
+vdprint_(const CHAR16 *fmt, const char *file, int line, const char *func,
+         va_list args)
 {
-	elf_va_list args2;
+	va_list args2;
 	EFI_STATUS efi_status = EFI_SUCCESS;
 
 	if (verbose) {
-		elf_va_copy(args2, args);
+		va_copy(args2, args);
 		console_print(L"%a:%d:%a() ", file, line, func);
 		efi_status = VPrint(fmt, args2);
-		elf_va_end(args2);
+		va_end(args2);
 	}
 	return efi_status;
 }
 
 EFI_STATUS EFIAPI
-VLogError(const char *file, int line, const char *func, const CHAR16 *fmt, elf_va_list args)
+VLogError(const char *file, int line, const char *func, const CHAR16 *fmt,
+          va_list args)
 {
-	elf_va_list args2;
+	va_list args2;
 	CHAR16 **newerrs;
 
 	newerrs = ReallocatePool(errs, (nerrs + 1) * sizeof(*errs),
@@ -38,11 +40,11 @@ VLogError(const char *file, int line, const char *func, const CHAR16 *fmt, elf_v
 	newerrs[nerrs] = PoolPrint(L"%a:%d %a() ", file, line, func);
 	if (!newerrs[nerrs])
 		return EFI_OUT_OF_RESOURCES;
-	elf_va_copy(args2, args);
+	va_copy(args2, args);
 	newerrs[nerrs+1] = VPoolPrint(fmt, args2);
 	if (!newerrs[nerrs+1])
 		return EFI_OUT_OF_RESOURCES;
-	elf_va_end(args2);
+	va_end(args2);
 
 	nerrs += 2;
 	newerrs[nerrs] = NULL;
@@ -54,12 +56,12 @@ VLogError(const char *file, int line, const char *func, const CHAR16 *fmt, elf_v
 EFI_STATUS EFIAPI
 LogError_(const char *file, int line, const char *func, const CHAR16 *fmt, ...)
 {
-	elf_va_list args;
+	va_list args;
 	EFI_STATUS efi_status;
 
-	elf_va_start(args, fmt);
+	va_start(args, fmt);
 	efi_status = VLogError(file, line, func, fmt, args);
-	elf_va_end(args);
+	va_end(args);
 
 	return efi_status;
 }
--- include/system/stdarg.h
+++ include/system/stdarg.h
@@ -8,24 +8,33 @@
 #ifndef _STDARG_H
 #define _STDARG_H
 
-#include <efistdarg.h>
+#ifndef GNU_EFI_USE_EXTERNAL_STDARG
+#define GNU_EFI_USE_EXTERNAL_STDARG
+#endif
+
+#if defined(__aarch64__) || defined(__arm__) || defined(__i386__) || \
+	defined(__i486__) || defined(__i686__) || defined(SHIM_UNIT_TEST)
+typedef __builtin_va_list va_list;
+#define va_copy(dest, start)  __builtin_va_copy(dest, start)
+#define va_start(marker, arg) __builtin_va_start(marker, arg)
+#define va_arg(marker, type)  __builtin_va_arg(marker, type)
+#define va_end(marker)        __builtin_va_end(marker)
+#elif defined(__x86_64__)
+typedef __builtin_ms_va_list va_list;
+#define va_copy(dest, start)  __builtin_ms_va_copy(dest, start)
+#define va_start(marker, arg) __builtin_ms_va_start(marker, arg)
+#define va_arg(marker, type)  __builtin_va_arg(marker, type)
+#define va_end(marker)        __builtin_ms_va_end(marker)
+#else
+#error what arch is this
+#endif
+
+typedef va_list VA_LIST;
+#define VA_COPY(dest, start)  va_copy(dest, start)
+#define VA_START(marker, arg) va_start(marker, arg)
+#define VA_END(marker)        va_end(marker)
+#define VA_ARG(marker, type)  va_arg(marker, type)
 
 #endif /* !_STDARG_H */
-#endif
-#ifndef SHIM_STDARG_H_
-#define SHIM_STDARG_H_
-
-typedef __builtin_ms_va_list ms_va_list;
-#define ms_va_copy(dest, start)  __builtin_ms_va_copy(dest, start)
-#define ms_va_start(marker, arg) __builtin_ms_va_start(marker, arg)
-#define ms_va_arg(marker, type)  __builtin_va_arg(marker, type)
-#define ms_va_end(marker)        __builtin_ms_va_end(marker)
-
-typedef __builtin_va_list elf_va_list;
-#define elf_va_copy(dest, start)  __builtin_va_copy(dest, start)
-#define elf_va_start(marker, arg) __builtin_va_start(marker, arg)
-#define elf_va_arg(marker, type)  __builtin_va_arg(marker, type)
-#define elf_va_end(marker)        __builtin_va_end(marker)
-
-#endif /* !SHIM_STDARG_H_ */
+#endif /* !SHIM_UNIT_TEST */
 // vim:fenc=utf-8:tw=75:noet
