8c0a47e1bee449db59527132f1107b67261a56d2
Peter Jones <pjones@redhat.com>
2021-07-26 17:29:15 -0400

    tests: Add config table support
    This adds a simple implementation of ST->ConfigurationTable,
    ST->NumberOfTableEntries, and BS->InstallConfigurationTable  to our test
    harness.
    
    Currently it is limited at 1024 entries, but that should be well more
    than enough for any tests we've currently considered.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- test-mock-variables.c
+++ test-mock-variables.c
@@ -8,6 +8,7 @@
 #include "mock-variables.h"
 
 #include <errno.h>
+#include <efivar/efivar.h>
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
@@ -15,6 +16,8 @@
 #include "test-data-efivars-0.h"
 
 #pragma GCC diagnostic ignored "-Wunused-label"
+
+static const size_t guidstr_size = sizeof("8be4df61-93ca-11d2-aa0d-00e098032b8c");
 
 void mock_print_guidname(EFI_GUID *guid, CHAR16 *name);
 void mock_print_var_list(list_t *head);
@@ -386,6 +389,421 @@ err:
 	return ret;
 }
 
+static void
+dump_config_table_if_wrong(const char * const func, int line, ...)
+{
+	va_list alist, blist;
+	bool okay = true;
+	size_t n = 0, m = 0;
+
+	va_start(alist, line);
+	va_copy(blist, alist);
+
+	int idx = va_arg(alist, int);
+	EFI_GUID *guid = va_arg(alist, EFI_GUID *);
+	while (idx >= 0 && guid != NULL) {
+		EFI_CONFIGURATION_TABLE *entry;
+		if (idx < 0)
+			goto nexta;
+
+		n += 1;
+		if (idx >= (int)ST->NumberOfTableEntries) {
+			okay = false;
+			goto nexta;
+		}
+
+		entry = &ST->ConfigurationTable[idx];
+		if (CompareGuid(guid, &entry->VendorGuid) != 0)
+			okay = false;
+
+nexta:
+		idx = va_arg(alist, int);
+		guid = va_arg(alist, EFI_GUID *);
+	}
+	va_end(alist);
+
+	if (okay)
+		return;
+
+	printf("%s:%d:%s(): %d entries:\n", __FILE__, line, func, ST->NumberOfTableEntries);
+	idx = va_arg(blist, int);
+	guid = va_arg(blist, EFI_GUID *);
+	while (idx >= 0 && guid != NULL) {
+		EFI_CONFIGURATION_TABLE *entry;
+
+		if (idx >= (int)ST->NumberOfTableEntries) {
+			printf("\t[%d]: invalid index for " GUID_FMT "\n",
+			       idx, GUID_ARGS(*guid));
+			goto nexta;
+		}
+
+		if (idx < 0) {
+			printf("\t[%d]: " GUID_FMT "\n", idx, GUID_ARGS(*guid));
+		} else {
+			entry = &ST->ConfigurationTable[idx];
+			printf("\t[%d]: %p ", idx, entry);
+			printf("{.VendorGuid:" GUID_FMT ",", GUID_ARGS(entry->VendorGuid));
+			printf("&.VendorTable:%p}\n", entry->VendorTable);
+			if (CompareGuid(guid, &entry->VendorGuid) != 0)
+				printf("\t\t\t  expected:" GUID_FMT "\n", GUID_ARGS(*guid));
+		}
+next:
+		idx = va_arg(blist, int);
+		guid = va_arg(blist, EFI_GUID *);
+	}
+	va_end(blist);
+
+	if ((int)ST->NumberOfTableEntries - n == 0)
+		return;
+
+	printf("%d extra table entries:\n", ST->NumberOfTableEntries - n);
+	for (m = n; m < ST->NumberOfTableEntries; m++) {
+		EFI_CONFIGURATION_TABLE *entry;
+
+		entry = &ST->ConfigurationTable[m];
+
+		printf("\t[%d]: %p ", m, entry);
+		printf("{.VendorGuid:" GUID_FMT ",", GUID_ARGS(entry->VendorGuid));
+		printf("&.VendorTable:%p}\n", entry->VendorTable);
+	}
+}
+
+static int
+test_install_config_table_0(void)
+{
+	int ret = -1;
+	EFI_STATUS status;
+
+	/*
+	 * These three guids are chosen on purpose: they start with "a",
+	 * "b", and "c", respective to their variable names, so you can
+	 * identify them when dumped.
+	 */
+	EFI_GUID aguid = SECURITY_PROTOCOL_GUID;
+	char astr[guidstr_size];
+	void *astrp = &astr[0];
+	int aidx = -1;
+
+	EFI_GUID bguid = EFI_HTTP_BINDING_GUID;
+	char bstr[guidstr_size];
+	void *bstrp = &bstr[0];
+	int bidx = -1;
+
+	EFI_GUID cguid = MOK_VARIABLE_STORE;
+	char cstr[guidstr_size];
+	void *cstrp = &cstr[0];
+	int cidx = -1;
+
+	EFI_GUID lip = LOADED_IMAGE_PROTOCOL;
+
+	EFI_GUID guids[3];
+
+	char tmpstr[guidstr_size];
+
+	sprintf(astrp, GUID_FMT, GUID_ARGS(aguid));
+	sprintf(bstrp, GUID_FMT, GUID_ARGS(bguid));
+	sprintf(cstrp, GUID_FMT, GUID_ARGS(cguid));
+
+	assert_equal_return(ST->NumberOfTableEntries, 0, -1, "%lu != %lu\n");
+
+	/*
+	 * test installing one
+	 */
+	status = BS->InstallConfigurationTable(&bguid, bstrp);
+	assert_equal_return(status, EFI_SUCCESS, -1, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 1, err, "%lu != %lu\n");
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[0].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[0].VendorGuid, &bguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+	assert_equal_goto(ST->ConfigurationTable[0].VendorTable,
+			  bstrp, err, "%p != %p\n");
+
+	/*
+	 * test re-installing the same one
+	 */
+	status = BS->InstallConfigurationTable(&bguid, bstrp);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 1, err, "%lu != %lu\n");
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[0].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[0].VendorGuid, &bguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+	assert_equal_goto(ST->ConfigurationTable[0].VendorTable,
+			  bstrp, err, "%p != %p\n");
+
+	/*
+	 * Test installing a second one
+	 */
+	status = BS->InstallConfigurationTable(&aguid, astrp);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 2, err, "%lu != %lu\n");
+
+	switch (mock_config_table_sort_policy) {
+		case MOCK_SORT_DESCENDING:
+			aidx = 1;
+			bidx = 0;
+			break;
+		case MOCK_SORT_PREPEND:
+			aidx = 0;
+			bidx = 1;
+			break;
+		case MOCK_SORT_APPEND:
+			aidx = 1;
+			bidx = 0;
+			break;
+		case MOCK_SORT_ASCENDING:
+			aidx = 0;
+			bidx = 1;
+			break;
+		default:
+			break;
+	}
+
+	dump_config_table_if_wrong(__func__, __LINE__,
+				   aidx, &aguid,
+				   bidx, &bguid,
+				   cidx, &cguid,
+				   -1, NULL);
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[aidx].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[aidx].VendorGuid, &aguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, astr);
+	assert_equal_goto(ST->ConfigurationTable[aidx].VendorTable, astrp,
+			  err, "%p != %p\n");
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[bidx].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[bidx].VendorGuid, &bguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+	assert_equal_goto(ST->ConfigurationTable[bidx].VendorTable, bstrp,
+			  err, "%p != %p\n");
+
+	/*
+	 * Test installing a third one
+	 */
+	status = BS->InstallConfigurationTable(&cguid, cstrp);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 3, err, "%lu != %lu\n");
+
+	switch (mock_config_table_sort_policy) {
+		case MOCK_SORT_DESCENDING:
+			aidx = 2;
+			bidx = 1;
+			cidx = 0;
+			break;
+		case MOCK_SORT_PREPEND:
+			aidx = 1;
+			bidx = 2;
+			cidx = 0;
+			break;
+		case MOCK_SORT_APPEND:
+			aidx = 1;
+			bidx = 0;
+			cidx = 2;
+			break;
+		case MOCK_SORT_ASCENDING:
+			aidx = 0;
+			bidx = 1;
+			cidx = 2;
+			break;
+		default:
+			break;
+	}
+
+	dump_config_table_if_wrong(__func__, __LINE__,
+				   aidx, &aguid,
+				   bidx, &bguid,
+				   cidx, &cguid,
+				   -1, NULL);
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[aidx].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[aidx].VendorGuid, &aguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, astr);
+	assert_equal_goto(ST->ConfigurationTable[aidx].VendorTable, astrp,
+			  err, "%p != %p\n");
+	memcpy(&guids[aidx], &aguid, sizeof(EFI_GUID));
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[bidx].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[bidx].VendorGuid, &bguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+	assert_equal_goto(ST->ConfigurationTable[bidx].VendorTable, bstrp,
+			  err, "%p != %p\n");
+	memcpy(&guids[bidx], &bguid, sizeof(EFI_GUID));
+
+	sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[cidx].VendorGuid));
+	assert_zero_goto(CompareGuid(&ST->ConfigurationTable[cidx].VendorGuid, &cguid),
+			 err, "%d != 0 (%s != %s)\n", tmpstr, cstr);
+	assert_equal_goto(ST->ConfigurationTable[cidx].VendorTable, cstrp,
+			  err, "%p != %p\n");
+	memcpy(&guids[cidx], &cguid, sizeof(EFI_GUID));
+
+	/*
+	 * Test removing NULL guid
+	 */
+	status = BS->InstallConfigurationTable(NULL, NULL);
+	assert_equal_goto(status, EFI_INVALID_PARAMETER, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 3, err, "%lu != %lu\n");
+
+	/*
+	 * Test removing a guid that's not present
+	 */
+	status = BS->InstallConfigurationTable(&lip, NULL);
+	assert_equal_goto(status, EFI_NOT_FOUND, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 3, err, "%lu != %lu\n");
+
+	/*
+	 * Test removing the middle one
+	 */
+	status = BS->InstallConfigurationTable(&guids[1], NULL);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 2, err, "%lu != %lu\n");
+
+	switch (mock_config_table_sort_policy) {
+		case MOCK_SORT_DESCENDING:
+			aidx = 1;
+			bidx = -1;
+			cidx = 0;
+			break;
+		case MOCK_SORT_PREPEND:
+			aidx = -1;
+			bidx = 1;
+			cidx = 0;
+			break;
+		case MOCK_SORT_APPEND:
+			aidx = -1;
+			bidx = 0;
+			cidx = 1;
+			break;
+		case MOCK_SORT_ASCENDING:
+			aidx = 0;
+			bidx = -1;
+			cidx = 1;
+			break;
+		default:
+			break;
+	}
+
+	dump_config_table_if_wrong(__func__, __LINE__,
+				   aidx, &aguid,
+				   bidx, &bguid,
+				   cidx, &cguid,
+				   -1, NULL);
+
+	if (aidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[aidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[aidx].VendorGuid, &aguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, astr);
+		assert_equal_goto(ST->ConfigurationTable[aidx].VendorTable, astrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[aidx], &aguid, sizeof(EFI_GUID));
+	}
+
+	if (bidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[bidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[bidx].VendorGuid, &bguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+		assert_equal_goto(ST->ConfigurationTable[bidx].VendorTable, bstrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[bidx], &bguid, sizeof(EFI_GUID));
+	}
+
+	if (cidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[cidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[cidx].VendorGuid, &cguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, cstr);
+		assert_equal_goto(ST->ConfigurationTable[cidx].VendorTable, cstrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[cidx], &cguid, sizeof(EFI_GUID));
+	}
+
+	/*
+	 * Test removing the lowest one
+	 */
+	status = BS->InstallConfigurationTable(&guids[0], NULL);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 1, err, "%lu != %lu\n");
+
+	switch (mock_config_table_sort_policy) {
+		case MOCK_SORT_DESCENDING:
+			aidx = 0;
+			bidx = -1;
+			cidx = -1;
+			break;
+		case MOCK_SORT_PREPEND:
+			aidx = -1;
+			bidx = 0;
+			cidx = -1;
+			break;
+		case MOCK_SORT_APPEND:
+			aidx = -1;
+			bidx = -1;
+			cidx = 0;
+			break;
+		case MOCK_SORT_ASCENDING:
+			aidx = -1;
+			bidx = -1;
+			cidx = 0;
+			break;
+		default:
+			break;
+	}
+
+	dump_config_table_if_wrong(__func__, __LINE__,
+				   aidx, &aguid,
+				   bidx, &bguid,
+				   cidx, &cguid,
+				   -1, NULL);
+
+	if (aidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[aidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[aidx].VendorGuid, &aguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, astr);
+		assert_equal_goto(ST->ConfigurationTable[aidx].VendorTable, astrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[aidx], &aguid, sizeof(EFI_GUID));
+	}
+
+	if (bidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[bidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[bidx].VendorGuid, &bguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, bstr);
+		assert_equal_goto(ST->ConfigurationTable[bidx].VendorTable, bstrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[bidx], &bguid, sizeof(EFI_GUID));
+	}
+
+	if (cidx >= 0) {
+		sprintf(tmpstr, GUID_FMT, GUID_ARGS(ST->ConfigurationTable[cidx].VendorGuid));
+		assert_zero_goto(CompareGuid(&ST->ConfigurationTable[cidx].VendorGuid, &cguid),
+				 err, "%d != 0 (%s != %s)\n", tmpstr, cstr);
+		assert_equal_goto(ST->ConfigurationTable[cidx].VendorTable, cstrp,
+				  err, "%p != %p\n");
+		memcpy(&guids[cidx], &cguid, sizeof(EFI_GUID));
+	}
+
+	/*
+	 * Test removing the last one
+	 */
+	status = BS->InstallConfigurationTable(&guids[0], NULL);
+	assert_equal_goto(status, EFI_SUCCESS, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 0, err, "%lu != %lu\n");
+
+	/*
+	 * Test removing it again
+	 */
+	status = BS->InstallConfigurationTable(&guids[0], NULL);
+	assert_equal_goto(status, EFI_NOT_FOUND, err, "%lx != %lx\n");
+	assert_equal_goto(ST->NumberOfTableEntries, 0, err, "%lu != %lu\n");
+
+	ret = 0;
+err:
+	while (ST->NumberOfTableEntries)
+		BS->InstallConfigurationTable(&ST->ConfigurationTable[0].VendorGuid, NULL);
+	mock_reset_config_table();
+
+	return ret;
+}
+
 int
 main(void)
 {
@@ -405,12 +823,15 @@ main(void)
 
 	for (int i = 0; i < MOCK_SORT_MAX_SENTINEL; i++) {
 		mock_variable_sort_policy = i;
+		mock_config_table_sort_policy = i;
 		printf("%s: setting variable sort policy to %s\n",
 		       program_invocation_short_name, policies[i]);
 
 		test(test_gnvn_buf_size_0);
 		test(test_gnvn_0);
 		test(test_gnvn_1);
+
+		test(test_install_config_table_0);
 	}
 
 	test(test_get_variable_0);
