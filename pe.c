de4392f7be95dd173059bf350426cc300f79454b
Peter Jones <pjones@redhat.com>
2022-05-03 17:05:20 -0400

    Also avoid CVE-2022-28737 in verify_image()
    PR 446 ("Add verify_image") duplicates some of the code affected by
    Chris Coulson's defense in depth patch against CVE-2022-28737 ("pe:
    Perform image verification earlier when loading grub").
    
    This patch makes the same change to the new function.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- pe.c
+++ pe.c
@@ -1038,7 +1038,28 @@ EFI_STATUS verify_image(void *data, unsigned int datasize,
 	}
 
 	/*
-	 * We only need to verify the binary if we're in secure mode
+	 * Perform the image verification before we start copying data around
+	 * in order to load it.
+	 */
+	if (secure_mode()) {
+		efi_status = verify_buffer(data, datasize,
+					   context, sha256hash, sha1hash);
+		if (EFI_ERROR(efi_status)) {
+			if (verbose)
+				console_print(L"Verification failed: %r\n", efi_status);
+			else
+				console_error(L"Verification failed", efi_status);
+			return efi_status;
+		} else if (verbose)
+			console_print(L"Verification succeeded\n");
+	}
+
+	/*
+	 * Calculate the hash for the TPM measurement.
+	 * XXX: We're computing these twice in secure boot mode when the
+	 *  buffers already contain the previously computed hashes. Also,
+	 *  this is only useful for the TPM1.2 case. We should try to fix
+	 *  this in a follow-up.
 	 */
 	efi_status = generate_hash(data, datasize, context, sha256hash,
 				   sha1hash);
@@ -1057,19 +1078,6 @@ EFI_STATUS verify_image(void *data, unsigned int datasize,
 		return efi_status;
 	}
 #endif
-
-	if (secure_mode()) {
-		efi_status = verify_buffer(data, datasize,
-					   context, sha256hash, sha1hash);
-		if (EFI_ERROR(efi_status)) {
-			if (verbose)
-				console_print(L"Verification failed: %r\n", efi_status);
-			else
-				console_error(L"Verification failed", efi_status);
-			return efi_status;
-		} else if (verbose)
-			console_print(L"Verification succeeded\n");
-	}
 
 	return EFI_SUCCESS;
 }
