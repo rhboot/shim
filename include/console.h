aaa3cdacb8cae0695a9fa6c4933ba563e92fde56
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-05-08 09:28:24 +0200

    Skip testing usleep()
    In some cases tests were failing due to a mismatch between our
    declaration of the usleep() function and what is being provided by
    unistd.h. This change simply makes our function declared only when not
    in a unit test environment.
    
    Signed-off-by: Kamil Aronowski <kamil.aronowski@yahoo.com>

--- include/console.h
+++ include/console.h
@@ -122,7 +122,9 @@ extern EFI_STATUS EFIAPI vdprint_(const CHAR16 *fmt, const char *file, int line,
 extern EFI_STATUS print_crypto_errors(EFI_STATUS rc, char *file, const char *func, int line);
 #define crypterr(rc) print_crypto_errors((rc), __FILE__, __func__, __LINE__)
 
+#ifndef SHIM_UNIT_TEST
 extern VOID usleep(unsigned long usecs);
+#endif
 
 /* This is used in various things to determine if we should print to the
  * console */
