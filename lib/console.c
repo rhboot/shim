f7a4338f1b5ef03dca83ce44075e9d6e5897e037
Kamil Aronowski <kamil.aronowski@yahoo.com>
2023-05-08 09:28:24 +0200

    Skip testing msleep()
    In preparation for renaming msleep() to usleep(), in some cases tests
    were failing due to a mismatch between our declaration of the usleep()
    function and what is being provided by unistd.h. This change simply
    makes our function declared only when not in a unit test environment.
    
    Signed-off-by: Kamil Aronowski <kamil.aronowski@yahoo.com>

--- lib/console.c
+++ lib/console.c
@@ -743,11 +743,13 @@ setup_verbosity(VOID)
 	setup_console(-1);
 }
 
+#ifndef SHIM_UNIT_TEST
 VOID
 msleep(unsigned long msecs)
 {
 	BS->Stall(msecs);
 }
+#endif
 
 /* This is used in various things to determine if we should print to the
  * console */
