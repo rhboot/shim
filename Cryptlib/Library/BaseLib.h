99eea5d4e7eb114ad12da38bf9b65f7bbbf69adc
Callum Farmer <gmbr3@opensuse.org>
2025-01-11 20:26:09 +0000

    Remove GNU_EFI_USE_EXTERNAL_STDARG

--- Cryptlib/Library/BaseLib.h
+++ Cryptlib/Library/BaseLib.h
@@ -1,10 +1,6 @@
 #if defined(__x86_64__)
 /* shim.h will check if the compiler is new enough in some other CU */
 
-#if !defined(GNU_EFI_USE_EXTERNAL_STDARG)
-#define GNU_EFI_USE_EXTERNAL_STDARG
-#endif
-
 #if !defined(GNU_EFI_USE_MS_ABI)
 #define GNU_EFI_USE_MS_ABI
 #endif
