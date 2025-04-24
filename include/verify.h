030e7df0407fc3fd89c28b0f9758de54e8522e65
Peter Jones <pjones@redhat.com>
2025-04-24 12:34:19 -0400

    Move a bunch of stuff from shim.c to verify.c
    This moves the authenticode validation functions from shim.c to
    verify.c.  Mostly I just wanted to stop adding clutter to shim.c, but
    also this should make adding unit tests for the verification functions
    much more straightforward later.  (Not planning of that in this patch
    set.)
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- include/verify.h
+++ include/verify.h
@@ -10,4 +10,31 @@
  */
 #pragma once
 
+EFI_STATUS
+verify_buffer (char *data, int datasize,
+	       PE_COFF_LOADER_IMAGE_CONTEXT *context,
+	       UINT8 *sha256hash, UINT8 *sha1hash);
+
+void
+init_openssl(void);
+
+/*
+ * Protocol v1 entry points.
+ */
+
+/*
+ * If secure boot is enabled, verify that the provided buffer is signed
+ * with a trusted key.
+ */
+EFI_STATUS
+shim_verify(void *buffer, UINT32 size);
+
+EFI_STATUS
+shim_hash(char *data, int datasize, PE_COFF_LOADER_IMAGE_CONTEXT *context,
+          UINT8 *sha256hash, UINT8 *sha1hash);
+
+EFI_STATUS
+shim_read_header(void *data, unsigned int datasize,
+                 PE_COFF_LOADER_IMAGE_CONTEXT *context);
+
 // vim:fenc=utf-8:tw=75:noet
