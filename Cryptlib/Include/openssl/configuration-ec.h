f3d596dee5f6cf207fabaae23573588ebf4c0a45
Peter Jones <pjones@redhat.com>
2026-06-11 11:33:00 -0400

    openssl: introduce OPENSSL_NO_PEM
    This mostly stops building PEM, since we never use it.  Saves over 9KiB.
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- Cryptlib/Include/openssl/configuration-ec.h
+++ Cryptlib/Include/openssl/configuration-ec.h
@@ -249,6 +249,9 @@ extern "C" {
 # ifndef OPENSSL_NO_PADLOCKENG
 #  define OPENSSL_NO_PADLOCKENG
 # endif
+# ifndef OPENSSL_NO_PEM
+#  define OPENSSL_NO_PEM
+# endif
 # ifndef OPENSSL_NO_PIE
 #  define OPENSSL_NO_PIE
 # endif
