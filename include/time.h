eb1d52ba55e248144aff89ee4a8919c023714d1b
Peter Jones <pjones@redhat.com>
2026-07-02 11:11:19 -0400

    shim: add some EFI_TIME helpers
    This adds some EFI_TIME versions of standard library time helpers to
    convert between "struct tm"<->EFI_TIME and time_t<->EFI_TIME:
    
      int efi_time_to_tm(EFI_TIME *src, struct tm *dest)
      int tm_to_efi_time(struct tm *src, EFI_TIME *dest, bool tzadj)
      EFI_TIME *efi_gmtime_r(time_t *src, EFI_TIME *dest)
      EFI_TIME *efi_gmtime(time_t *src)
      time_t efi_mktime(EFI_TIME *src)
    
    Signed-off-by: Peter Jones <pjones@redhat.com>

--- include/time.h
+++ include/time.h
@@ -6,4 +6,18 @@
 
 #pragma once
 
+/*
+ * If we're building unit tests, then all of the structs for time will come
+ * from glibc, and all of these helpers are in libefivar.
+ */
+#ifndef SHIM_UNIT_TEST
+#include "Cryptlib/Include/CrtLibTime.h"
+
+extern int efi_time_to_tm(const EFI_TIME * const s, struct tm *d);
+extern int tm_to_efi_time(const struct tm *const s, EFI_TIME *d, bool tzadj);
+extern EFI_TIME *efi_gmtime_r(const time_t *time, EFI_TIME *result);
+extern EFI_TIME *efi_gmtime(const time_t *time);
+extern time_t efi_mktime(const EFI_TIME * const time);
+#endif
+
 // vim:fenc=utf-8:tw=75:noet
